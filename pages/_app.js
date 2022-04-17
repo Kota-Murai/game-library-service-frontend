7bd9fea4add2f3942c916f00020a5f1fca212dab
Kota Murai <toppomurai@gmail.com>
2022-04-18 05:09:08 +0900

    _app.jsと_document.jsをmui5向けに修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,59 +1,43 @@
-// import '../styles/global.css'
-import {Provider} from 'react-redux';
-import createStore from '../src/store/store';
-
-
-export const store = createStore();
-
-// export default function App({ Component, pageProps }) {
-//   return (
-//     <>
-//       <Provider store={store}>
-//         <Component {...pageProps} />
-//       </Provider>
-//     </>
-//   )
-// }
-
-import React from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+
+// redux
+import {Provider} from 'react-redux';
+import createStore from '../src/store/store';
+export const store = createStore();
+
+
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
     <Provider store={store}>
-    <React.Fragment>
-      
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-      
-    </React.Fragment>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </Provider>
   );
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
   pageProps: PropTypes.object.isRequired,
 };
